291aeca1b39c6293a3a841147f6ad1c3c8fc54b2
sameeraprakash9-ship-it <sameeraprakash9@gmail.com>
2026-09-15 22:58:58 -0700

    prakashgemmeli

---
+++ day1.js
@@ -0,0 +1,72 @@
+// // // let marks = 100;
+// // // if (marks = 90){
+// // // console.log("GRAD A")
+// // // }
+
+
+// // // let number =10;
+// // // if( number> 10){
+// // //     console.log("the number is greater than 10");
+
+// // // }else{
+// // //     console.log("the number is not greater than 10")
+// // // }
+
+
+// // // let number = 20;
+// // // if ( number >20){
+// // //     console.log("the number is greater than 20");
+
+// // // }else{
+// // //     console.log("the number is greater than 20");
+// // // }
+
+// // let number = 20;
+// // if ( number> 20);{
+// //     console.log("the number is greater than 20 ");
+
+// // }else{
+// // console.log("the number is greater than 20");
+// // }
+
+// // let number = 10;
+// // if("number > 10"){
+// //     console.log("the number is greater than 10");
+// // }else{
+// //     console.log("the number is greater than 10");
+// // }
+
+// let num = 100
+// if(num<=100){
+// console.log("the number is less than ")
+// }
+
+
+
+// let num = 100
+// if(num <=100){
+//     console.log("the number is less than ")
+// }
+
+const bookings = 0
+const coupon = "ABHI10%"
+const draw =23456
+if(bokking === 0){
+    if(coupon=== "ABHI10%"){
+        IF()
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
